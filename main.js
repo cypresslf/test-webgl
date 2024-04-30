fbb7d5d6fba170c922a823469932951cad45e1dc
Cypress <cypress@lumafield.com>
2024-04-30 18:16:26 -0400

    Use a video as a texture

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { drawScene } from "./draw-scene.js";
 import { initBuffers } from "./init-buffers.js";
 
+let copyVideo = false;
+
 main();
 
 function main() {
@@ -74,7 +76,8 @@ function main() {
   };
 
   const buffers = initBuffers(gl);
-  const texture = loadTexture(gl, "cube-texture.png");
+  const texture = initTexture(gl);
+  const video = setupVideo("Firefox.mp4");
   // Flip image pixels into the bottom-to-top order that WebGL expects.
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
   if (buffers.position === null) {
@@ -95,6 +98,9 @@ function main() {
     now *= 0.001; // convert to seconds
     deltaTime = now - then;
     then = now;
+    if (copyVideo) {
+      updateTexture(gl, texture, video);
+    }
     drawScene(gl, shaderInfo, buffers, texture, cubeRotation);
     cubeRotation += deltaTime;
     requestAnimationFrame(render);
@@ -176,60 +182,74 @@ function loadShader(gl, type, source) {
 
 /**
  * @param {WebGLRenderingContext} gl
- * @param {string} url
  */
-function loadTexture(gl, url) {
+function initTexture(gl) {
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
-  const level = 0;
-  const internalFormat = gl.RGBA;
-  const width = 1;
-  const height = 1;
-  const border = 0;
-  const srcFormat = gl.RGBA;
-  const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+
+  // Because video has to be download over the internet
+  // they might take a moment until it's ready so
+  // put a single pixel in the texture so we can
+  // use it immediately.
   gl.texImage2D(
     gl.TEXTURE_2D,
-    level,
-    internalFormat,
-    width,
-    height,
-    border,
-    srcFormat,
-    srcType,
-    pixel
+    0,
+    gl.RGBA,
+    1,
+    1,
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    new Uint8Array([0, 0, 255, 255])
   );
-  const image = new Image();
-  image.onload = () => {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      level,
-      internalFormat,
-      srcFormat,
-      srcType,
-      image
-    );
-    // WebGL1 has different requirements for power of 2 images
-    // vs. non power of 2 images so check if the image is a
-    // power of 2 in both dimensions.
-    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-      // Yes, it's a power of 2. Generate mips.
-      gl.generateMipmap(gl.TEXTURE_2D);
-    } else {
-      // No, it's not a power of 2. Turn off mips and set
-      // wrapping to clamp to edge
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    }
-  };
-  image.src = url;
+
+  // Turn off mips and set wrapping to clamp to edge so it
+  // will work regardless of the dimensions of the video.
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
   return texture;
 }
 
-/** @param {number} value */
-function isPowerOf2(value) {
-  return (value & (value - 1)) === 0;
+/**
+ * @param {WebGLRenderingContext} gl
+ * @param {WebGLTexture} texture
+ * @param {HTMLVideoElement} video
+ */
+function updateTexture(gl, texture, video) {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
+}
+
+/** @param {string} url */
+function setupVideo(url) {
+  const video = document.createElement("video");
+  const checkReady = () =>
+    playing && timeupdate ? (copyVideo = true) : undefined;
+  let playing = false;
+  let timeupdate = false;
+  video.playsInline = true;
+  video.muted = true;
+  video.loop = true;
+
+  video.addEventListener(
+    "playing",
+    () => {
+      playing = true;
+      checkReady();
+    },
+    true
+  );
+  video.addEventListener(
+    "timeupdate",
+    () => {
+      timeupdate = true;
+      checkReady();
+    },
+    true
+  );
+  video.src = url;
+  video.play();
+  return video;
 }
